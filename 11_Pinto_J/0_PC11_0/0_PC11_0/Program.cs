d8d8a2a8fc64d51edd7d8f2f5945e2072fc236b5
Ltotnip <leonelpinto772@gmail.com>
2025-04-24 14:26:06 -0300

    Add files via upload

---
+++ 11_Pinto_J/0_PC11_0/0_PC11_0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC11_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             TP 0 - Etapa 0
+             Desarrollar un programa que Imprima por pantalla "hola mundo!"
+             */
+            Console.WriteLine("Hola Mundo!"); // Imprimo
+            Console.ReadKey(); // Espero una tecla
+        }
+    }
+}
