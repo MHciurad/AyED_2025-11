f771e49064457ee8c0a78fe2cb71fc81e9ef8a75
Ltotnip <leonelpinto772@gmail.com>
2025-04-24 14:34:38 -0300

    Add files via upload

---
+++ 11_Daruich_M/0_PC11_4/0_PC11_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC11_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { /*Escribe un programa en C# que calcule
+            el resultado de sumar dos números introducidos
+            por el usuario.*/
+
+            int pre;
+            int pri;
+            int res;
+            Console.WriteLine("introduzca num 1");
+            pre = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduzca num 2");
+            pri = int.Parse(Console.ReadLine());
+            res = pri + pre;
+            Console.WriteLine("su resultado es " + res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11_Daruich_M/0_PC11_7/0_PC11_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC11_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Escribe un programa en C# que calcule el resultado de 
+            dividr dos números introducidos por el usuario.*/
+            int pre;
+            int pri;
+            int res;
+            Console.WriteLine("introduzca num 1");
+            pre = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduzca num 2");
+            pri = int.Parse(Console.ReadLine());
+            res = pre / pri;
+            Console.WriteLine("su resultado es " + res);
+            Console.ReadKey();
+        }
+    }
+}
