c013f6d47b0d8bef13735ea38706b627540a4887
MHciurad <mndaruich28@gmail.com>
2025-06-03 15:48:05 -0300

    TP 2_6
    TP terminado

---
+++ 11_Daruich_M/2_PC11_6/2_PC11_6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_PC11_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Examinados, Multiplo;
+
+            Console.WriteLine("indique cuantos examinados hubo");
+            Examinados = int.Parse(Console.ReadLine());
+            int[] puntajes = new int[Examinados];
+            for (int conta = 0; conta < Examinados; conta++)
+            {
+                Console.WriteLine("ingrese el puntaje del examinado " + (conta + 1));
+                puntajes[conta] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(puntajes);
+            Console.WriteLine("indique por cual multiplo quiere filtrar");
+            Multiplo = int.Parse(Console.ReadLine());
+            for (int conta = 0; conta <  Examinados; conta++)
+            {   if (puntajes[conta]%Multiplo == 0)
+                {
+                    Console.WriteLine(puntajes[conta]);
+                }
+                Console.ReadKey();
+                }
+
+
+
+            }
+    }
+}
