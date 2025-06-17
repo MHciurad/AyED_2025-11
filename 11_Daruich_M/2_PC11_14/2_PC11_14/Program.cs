2342e2e6f20b9b25c32f2f6cdf2c8a7b6e9c6cbe
MHciurad <mndaruich28@gmail.com>
2025-06-17 16:33:12 -0300

    Update Program.cs

--- 11_Daruich_M/2_PC11_14/2_PC11_14/Program.cs
+++ 11_Daruich_M/2_PC11_14/2_PC11_14/Program.cs
@@ -10,15 +10,25 @@ namespace _2_PC11_14
     {
         static void Main(string[] args)
         {
-            string[,] Pochidex = new string[75, 5];
-            int op, OP, z2, z1, columnaALlenar;
+            string[,] Pochidex = new string[75, 6];
+            int op, OP, z2, z1, colen, colen2, colen3;
             z1 = 0;
             z2 = 0;
             op = 1;
-            columnaALlenar = 0;
+            colen = 0;
+            colen2 = 5;
+            colen3 = 4;
             for (int i = 0; i < 75; i++)
             {
-                Pochidex[i, columnaALlenar] = (i + 1).ToString();
+                Pochidex[i, colen] = 0.ToString();
+            }
+            for (int i = 0; i < 75; i++)
+            {
+                Pochidex[i, colen2] = 0.ToString();
+            }
+            for (int i = 0; i < 75; i++)
+            {
+                Pochidex[i, colen3] = 0.ToString();
             }
             Pochidex[0, 0] = "fila";
             Pochidex[0, 1] = "nombre";
@@ -31,7 +41,7 @@ namespace _2_PC11_14
             while (op != 9)
             {
                 op = 0;
-                z2 = 0;
+                z2 = 1;
                 Console.WriteLine("1. Registrar Pochimon");
                 Console.WriteLine("2. Asignar investigador Pochimon");
                 Console.WriteLine("3. Actualizar Nivel de Pochimon");
@@ -56,13 +66,12 @@ namespace _2_PC11_14
                         Console.WriteLine("introduzca nivel");
                         Pochidex[z1, z2] = Console.ReadLine();
                         z2 = z2 + 1;
-                        Console.ReadKey();
                         break;
 
                     case 2:
                         for (int r = 0; r < 75; r++)
                         {
-                            for (int c = 0; c < 5; c++)
+                            for (int c = 0; c < 6; c++)
                             {
                                 Console.Write($"[{Pochidex[r, c],-5}]");
                             }
@@ -74,7 +83,7 @@ namespace _2_PC11_14
                         Console.WriteLine("cual Pochimon Quiere actualizar?");
                         OP = int.Parse(Console.ReadLine());
                         Console.WriteLine("a que nivel a cambiado?");
-                        Pochidex[OP, 4] = Console.ReadLine();
+                        Pochidex[OP, 3] = Console.ReadLine();
                         break;
                     case 4:
 
